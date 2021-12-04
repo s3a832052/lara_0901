d68e549d00e071793fabf7d00f33064ca0a4ec9b
GuoTingRui <azsxdc890902@gmail.com>
2021-12-04 16:01:52 +0800

    練習8:前台路由

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,17 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts=Post::orderBy('created_at','DESC')->get();
+        $data=['posts'=>$posts];
+        return view('posts.index',$data);
     }
 
     public function show($id)
     {
-        $data = ['id' => $id];
-
-        return view('posts.show', $data);
+        $post=Post::find($id);
+        $data=['post'=>$post];
+        return view('posts.show',$data);
     }
 }
